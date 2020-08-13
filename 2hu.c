ef720ac9eaebe7e0dc6e7c95ea188c6ff05e6676
nakagawaouran <kdfizz118@gmail.com>
2020-08-13 14:29:45 -0400

    adds shooting and movement

--- 2hu.c
+++ 2hu.c
@@ -3,7 +3,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include "datatypes.h"
-#include "unistd.h"
+#include <unistd.h>
 
 //Dimensions of the logical render size, Timing for game logic
 const int WIDTH = 640, HEIGHT = 480;
@@ -37,12 +37,15 @@ int main(int argc, char const *argv[]) {
   SDL_Surface *temp = SDL_LoadBMP("test.bmp");
   player.texture = SDL_CreateTextureFromSurface(renderMain, temp);
   SDL_FreeSurface(temp);
+
+  bulletSprites[0].width = loadTransparent(renderMain, "1.bmp", &bulletSprites[0].texture);
+
   chdir("..");
 
   //Main Game Loop
-  int running = 1;
+  int gameRunning = 1;
   SDL_Event event;
-  while(running){
+  while(gameRunning){
 
 
     SDL_SetRenderDrawColor(renderMain, 23, 23 ,23, 0xFF);
@@ -50,9 +53,11 @@ int main(int argc, char const *argv[]) {
 
     updatePlayer(player, keyState);
     renderPlayer(renderMain, player);
+    renderBullets(sizeof(activePlayerBullets)/sizeof(activePlayerBullets[0]), renderMain, activePlayerBullets);
+    renderGUI(renderMain, player);
     SDL_RenderPresent(renderMain);
 
-    printf("UP:%d DOWN:%d LEFT:%d RIGHT:%d SHOOT:%d\n",keyState.up,keyState.down,keyState.left,keyState.right,keyState.shoot);
+  //  printf("UP:%d DOWN:%d LEFT:%d RIGHT:%d SHOOT:%d\n",keyState.up,keyState.down,keyState.left,keyState.right,keyState.shoot);
 
 
     //Get User Input
@@ -65,7 +70,7 @@ int main(int argc, char const *argv[]) {
         exit(1);
         break;
 
-      //Pressing down a keyboard key
+      //Pressing down a keyboard key and setting the key manager to the correct state
       case SDL_KEYDOWN:
         if(event.key.keysym.sym ==  SDLK_UP){
           keyState.up = 1;}
@@ -81,6 +86,9 @@ int main(int argc, char const *argv[]) {
 
         if(event.key.keysym.sym ==  SDLK_z){
           keyState.shoot = 1;}
+
+        if(event.key.keysym.sym ==  SDLK_LSHIFT){
+          keyState.focus = 1;}
 
       break;
 
@@ -100,6 +108,9 @@ int main(int argc, char const *argv[]) {
 
         if(event.key.keysym.sym ==  SDLK_z){
           keyState.shoot = 0;}
+
+        if(event.key.keysym.sym ==  SDLK_LSHIFT){
+          keyState.focus = 0;}
 
       break;
 
@@ -121,8 +132,9 @@ int main(int argc, char const *argv[]) {
 }
 
 void updatePlayer(struct PlayerData p, struct KeyDownState ks){
-  //Decrease Invinciblity Frame
+  //Decrease Invinciblity Frame each tick + 1 tick closer to being able to shoot
   player.iframe--;
+  player.bulletCD--;
 
   //Move player, cut velocity in half if they are moving in 2 directions at once
   int tempX = 0, tempY = 0, mod = 0;
@@ -141,8 +153,13 @@ void updatePlayer(struct PlayerData p, struct KeyDownState ks){
   if(mod == 2){
     tempX*=0.5;
     tempY*=0.5;}
+  if(ks.focus == 1){
+    tempX*=0.5;
+    tempY*=0.5;}
   player.rect.x+=tempX;
   player.rect.y+=tempY;
+
+  //printf("tempx:%d tempy:%d\n",tempX,tempY);
 
   //Check if player is out of bounds, if they are then set them to the borders
   //The horizontal borders are intentionally set to be equal to height to create a square playing area
@@ -157,6 +174,14 @@ void updatePlayer(struct PlayerData p, struct KeyDownState ks){
     player.rect.y = HEIGHT - player.rect.h;
   }
 
+  //Check to see if the player can shoot, they have to be holding down shoot key plus not be on a cooldown
+  if(player.bulletCD <= 0 && ks.shoot == 1){
+    struct BulletData temp = { {player.rect.x + ( player.rect.w / 2 ) - 8, player.rect.y, 16, 16 }, 0, -5, 0, -20, 0, 0 ,0, 1};
+    createBullet(sizeof(activePlayerBullets)/sizeof(activePlayerBullets[0]), activePlayerBullets, temp);
+
+    player.bulletCD = 10;
+  }
+
 }
 
 void renderPlayer(SDL_Renderer *r, struct PlayerData p){
@@ -165,4 +190,59 @@ void renderPlayer(SDL_Renderer *r, struct PlayerData p){
   if(p.iframe < 0 || p.iframe % 2 == 0){
     SDL_RenderCopy(r, p.texture, NULL, &p.rect);
   }
+}
+
+void renderGUI(SDL_Renderer *r, struct PlayerData p){
+  SDL_SetRenderDrawColor(r,124,124,124,0xFF);
+  SDL_Rect sidebar = {HEIGHT, 0, WIDTH-HEIGHT, HEIGHT};
+  SDL_RenderFillRect(r, &sidebar);
+}
+
+void renderBullets(int size, SDL_Renderer *r, struct BulletData activeList[]){
+  for(int i = 0; i < size; i++){
+    if(activeList[i].alive == 1){
+      //Move the bullets
+      activeList[i].rect.x+=activeList[i].velX;
+      activeList[i].rect.y+=activeList[i].velY;
+
+      //Check if bullets are out bounds
+      SDL_Rect checkBounds = activeList[i].rect;
+      if(checkBounds.x < 0 - checkBounds.w){
+        activeList[i].alive = 0;
+      }else if(checkBounds.x > HEIGHT){
+        activeList[i].alive = 0;
+      }
+      if(checkBounds.y < 0 - checkBounds.h){
+        activeList[i].alive = 0;
+      }else if(checkBounds.y > HEIGHT){
+        activeList[i].alive = 0;
+      }
+
+      if (activeList[i].alive == 1){
+      SDL_RenderCopy(r, bulletSprites[activeList[i].textureID].texture, NULL, &activeList[i].rect);
+    }
+  }
+  }
+}
+
+void createBullet(int size, struct BulletData activeList[], struct BulletData bulletData){
+  for(int i = 0; i < size; i++){
+    if(activeList[i].alive != 1){
+      activeList[i] = bulletData;
+      activeList[i].alive = 1;
+      return 0;
+    }
+  }
+}
+
+int loadTransparent(SDL_Renderer *r, char path[], SDL_Texture **target){
+  SDL_Surface *temp;
+  int val = 0;
+  if((temp = SDL_LoadBMP(path)) != NULL){
+    SDL_SetColorKey(temp, SDL_TRUE, SDL_MapRGB(temp->format, 0xFF, 0, 0xFF) );
+    *target = SDL_CreateTextureFromSurface(r, temp);
+    val = temp->w / 8;
+  }
+  SDL_FreeSurface(temp);
+  return val;
 }
--- datatypes.h
+++ datatypes.h
@@ -1,9 +1,21 @@
+//Manage the state of whether the keys are being held or not, a value of 1 for true, 0 false
 struct KeyDownState{
-  int up, down, left, right, shoot;
+  int up, down, left, right, shoot, focus;
 }keyState;
 
+//All the variables the player needs, playerClean are the defaults given at the start of a new game
 struct PlayerData{
   int vel, lives, bombs, level, xp, iframe, bulletCD, bombCD;
   SDL_Texture *texture;
   SDL_Rect rect;
-}player, playerClean = {6, 3, 3, 0, 0, 0, 0, 0, NULL, {0, 0, 64, 64} };
+}player, playerClean = {4, 3, 3, 0, 0, 0, 0, 0, NULL, {0, 0, 48, 48} };
+
+struct BulletGraphics{
+  SDL_Texture *texture;
+  int width;
+}bulletSprites[99];
+
+struct BulletData{
+  SDL_Rect rect;
+  int velX, velY, framePos, damage, textureID, timer, alive, id;
+}activePlayerBullets[99], activeEnemyBullets[300];
